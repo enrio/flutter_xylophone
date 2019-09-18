3e2e02e8ea17d1fc2128330fc8836d7c30fdb548
ThucNV <nvthuc@outlook.com>
2019-09-18 15:31:02 +0700

    Finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,61 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        // appBar: AppBar(
+        //   title: Text("Piano"),
+        // ),
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+              XylophoneButton(Colors.red[500], 1),
+              XylophoneButton(Colors.purple[500], 2),
+              XylophoneButton(Colors.indigo[500], 3),
+              XylophoneButton(Colors.lightBlue[500], 4),
+              XylophoneButton(Colors.teal, 5),
+              XylophoneButton(Colors.lightGreen, 6),
+              XylophoneButton(Colors.yellowAccent, 7),
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class XylophoneButton extends StatelessWidget {
+  static AudioCache player = new AudioCache();
+  final Color color;
+
+  final int num;
+
+  XylophoneButton(this.color, this.num);
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: FlatButton(
+        padding: EdgeInsets.all(0),
+        child: Container(
+          color: color,
+        ),
+        onPressed: () {
+          onPressedColor(num);
+        },
+      ),
+    );
+  }
+
+  onPressedColor(int i) async {
+    print("Pressed $i");
+    // final player = new AudioCache();
+    player.play('note$i.wav');
   }
 }
